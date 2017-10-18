e954f9171987a1b5fd731a6cd979f9bb7450e8a3
icanstudioz <icanstudioz@icanstudiozs-Mac-mini.local>
2017-10-18 17:16:08 +0530

    change

---
+++ OrganizationsVolunteer/MVC/View/TableViewCell/expandCell.swift
@@ -0,0 +1,26 @@
+//
+//  expandCell.swift
+//  Flight
+//
+//  Created by Apple on 23/08/17.
+//  Copyright © 2017 icanstudioZ. All rights reserved.
+//
+
+import UIKit
+
+class expandCell: UITableViewCell {
+
+    @IBOutlet weak var descriptionview: UILabel!
+    @IBOutlet weak var bg_view: UIView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
